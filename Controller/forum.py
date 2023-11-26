7a9360bbd8c8b9923734afe587b950d0f0db6331
xYarvinx <otebator@gmail.com>
2023-11-26 18:36:54 +1000

    Delete request for posts

--- Controller/forum.py
+++ Controller/forum.py
@@ -8,11 +8,11 @@ from Model.models import User, ForumPost, Comment
 
 forum = Blueprint('forum', __name__)
 
-@forum.route('/forum/<int:forum_id>', methods=['GET', 'POST'])
-def forum_details(forum_id):
-    forum_data = ForumPost.query.get(forum_id)
+@forum.route('/forum/<int:post_id>', methods=['GET', 'POST'])
+def forum_details(post_id):
+    forum_data = ForumPost.query.get(post_id)
     user_data = User.query.get(forum_data.user_id)
-    comments = (db.session.query(Comment, User).filter_by(post_id=forum_id).filter_by(user_id=User.id)
+    comments = (db.session.query(Comment, User).filter_by(post_id=post_id).filter_by(user_id=User.id)
                 .order_by(-Comment.id).values(User.username, Comment.text,
                                               User.id.label('user_id'), Comment.id.label('comment_id')))
 
@@ -22,7 +22,7 @@ def forum_details(forum_id):
         comment = Comment(text=form.content.data, user_id=current_user.id, post_id=forum_data.id)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('forum.forum_details', forum_id=forum_data.id))
+        return redirect(url_for('forum.forum_details', post_id=forum_data.id))
 
     return render_template('forum_details.html', forum=forum_data, user=user_data,
                            comments=comments, form=form)
@@ -75,3 +75,27 @@ def delete_comment(comment_id):
     db.session.delete(comment)
     db.session.commit()
     return redirect(url_for('forum.forum_details', forum_id=post.id))
+
+
+@login_required
+@forum.route('/forum/<int:post_id>/delete_post', methods=['POST', 'GET'])
+def delete_post(post_id):
+    if request.method == 'GET':
+        post = ForumPost.query.get_or_404(post_id)
+        if current_user.id != post.user_id:
+            return redirect(url_for('forum.forum_details', forum_id=post.id))
+
+        # Querying and deleting comments related to the post
+        comments = Comment.query.filter_by(post_id=post_id)
+        for comment in comments:
+            db.session.delete(comment)
+
+        # Deleting the post
+        db.session.delete(post)
+        db.session.commit()
+
+        return redirect(url_for('forum.show_forum'))
+
+    return redirect(url_for('forum.show_forum'))
+
+
